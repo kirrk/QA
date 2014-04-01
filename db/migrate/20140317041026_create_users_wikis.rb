1e6d803a40d7c3a932f8b0e13085365ef48da72f
Karen Kirr <kkirr77@yahoo.com>
2014-04-01 08:01:17 -0700

    update migration

--- db/migrate/20140317041026_create_users_wikis.rb
+++ db/migrate/20140317041026_create_users_wikis.rb
@@ -1,11 +1,12 @@
 class CreateUsersWikis < ActiveRecord::Migration
   def change
     create_table :users_wikis, :id => false do |t|
-      t.references :user, :wiki
+      t.integer "user_id"
+      t.integer "wiki_id"
     end
 
-    add_index :users_wikis, [:user_id, :wiki_id],
-      name: "users_wikis_index",
+    add_index :users_wikis, ["user_id", "wiki_id"],
+      #name: "users_wikis_index",
       unique: true
   end
 end
